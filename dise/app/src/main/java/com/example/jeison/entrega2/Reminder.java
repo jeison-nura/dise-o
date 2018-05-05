34bc44b188fbf11544f399d5eddd4a53208a6ec2
jeison <jeisonnoc@gmail.com>
2018-05-05 13:43:56 -0500

    listo

---
+++ dise/app/src/main/java/com/example/jeison/entrega2/Reminder.java
@@ -0,0 +1,25 @@
+package com.example.jeison.entrega2;
+
+/**
+ * Created by jorge_000 on 1/05/2018.
+ */
+
+public class Reminder {
+    private String name;
+
+    public Reminder() {
+    }
+
+    public Reminder(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ dise/app/src/main/java/com/example/jeison/entrega2/ReminderAdapter.java
@@ -0,0 +1,87 @@
+package com.example.jeison.entrega2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.jeison.entrega2.R;
+import com.example.jeison.entrega2.Reminder;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Reminder> reminderList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        private View elementView;
+
+
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.info);
+            elementView = view;
+        }
+    }
+
+
+    public ReminderAdapter(Context mContext, List<Reminder> reminderList) {
+        this.mContext = mContext;
+        this.reminderList = reminderList;
+    }
+
+
+    //@Override
+    //public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    //    View itemView = LayoutInflater.from(parent.getContext())
+    //            .inflate(R.layout.reminder_card, parent, false);
+    //
+    //    return new MyViewHolder(itemView);
+    //}
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminder_r_view,null,false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        final Reminder reminder = reminderList.get(position);
+
+        holder.elementView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(mContext,"Click seleccionando: "+serie.getName(),Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(mContext,Main2Activity.class );
+                //intent.putExtra("name", serie.getName());
+                //intent.putExtra("numOfSongs", serie.getNumOfSeasons());
+                //intent.putExtra("thumbnail", serie.getThumbnail());
+                //mContext.startActivity(intent);
+            }
+        });
+        holder.title.setText(reminder.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return reminderList.size();
+    }
+}
